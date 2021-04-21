9fc0c9632c81097b11a7c08791fba0a9879207ce
ura43 <uraharasama123@gmail.com>
2021-04-21 03:46:42 +0200

    added basic youtube-dl functionality

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 print("Baaah")
+import youtube_dl
+
+class MyLogger(object):
+	def debug(self, msg):
+		pass
+	def warning(self, msg):
+		pass
+	def error(self, msg):
+		print(msg)
+
+def my_hook(d):
+	if d['status'] == 'finished':
+		print('Done downloading, now converting ...')
+
+ydl_opts = {
+	'format': 'best',
+#	'postprocessors': [{
+#		'key': 'FFmpegExtractAudio',
+#		'preferredcodec': 'mp3',
+#		'preferredquality': '192'
+#	}],
+	'logger': MyLogger(),
+	'progress_hooks': [my_hook]
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+	ydl.download(['https://youtu.be/ej9N6cJovwQ'])
+
+
+
+
+
